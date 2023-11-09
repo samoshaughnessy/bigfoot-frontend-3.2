ea640c68d8b7eac5d1e93116f4221585b4d8ff43
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2023-11-09 11:07:17 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import logo from "/logo.png";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={logo} className="logo react" alt="React logo" />
+      </div>
+      <h1>Bigfoot Frontend </h1>
+      <div className="card">
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
